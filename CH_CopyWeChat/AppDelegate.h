355dc17e8ad18da48d51ad88c866e94959bba2e9
耗子 <koshi@haozis-mac-mini.local>
2016-06-16 22:03:04 +0800

    Initial Commit

---
+++ CH_CopyWeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CH_CopyWeChat
+//
+//  Created by 耗子 on 16/6/16.
+//  Copyright © 2016年 DogCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
