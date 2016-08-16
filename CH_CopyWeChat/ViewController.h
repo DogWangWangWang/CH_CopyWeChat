ae0b26b6c32283982d4702cdd288c5e11f2ed81e
DogWangWangWang <koshi@haozis-Mac-mini.local>
2016-08-16 13:58:17 +0800

    git@github.com:DogWangWangWang/CH_CopyWeChat.git
    hahaha

---
+++ CH_CopyWeChat/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CH_ClockDemo
+//
+//  Created by 耗子 on 16/8/9.
+//  Copyright © 2016年 DogCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CH_CopyWeChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CH_ClockDemo
+//
+//  Created by 耗子 on 16/8/9.
+//  Copyright © 2016年 DogCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
